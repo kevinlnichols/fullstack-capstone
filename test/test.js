14763437f17d629642a93010d4a0cdcdbe17d243
Kevin Nichols <kevinlnichols@gmail.com>
2017-11-01 12:37:41 -0400

    minor change

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
+
+const should = chai.should();
+const app = server.app;
+const storage = server.storage;
+
+chai.use(chaiHttp);
+
+describe('index', function() {
+    it('exists', function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+});
