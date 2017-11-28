3591f0148401471c76d91811ddd1fe5239e8f61a
Kevin Nichols <kevinlnichols@gmail.com>
2017-11-28 16:17:16 -0500

    updated view test

--- public/scripts/tests/view.js
+++ public/scripts/tests/view.js
@@ -1,25 +1,53 @@
 const viewTests = () => {
-    $('#dropdown-menu').click(event => {
+    $('#dropdown-menu').one('click', event => {
         console.log('hi');
         event.preventDefault();  
-        let options = '';
-        $('#dropdown-menu').each(function(value) {
-            options += `<option value = "${value}">${value}</option>`;
-        });
-        $('#dropdown-menu').append(options);
         $.ajax({
             url: '/tests/list',
             type: 'get',
             contentType: 'application/json',
             success: function(data) {
+                /*let options = '';
+                $('#dropdown-menu').each(function(value) {
+                    options += `<option value="${this.value}">${this.value}</option>`;
+                });*/
+               
+                    let options = '';
+                    for (i=0; i < data.length; i++) {
+                        console.log(data);
+                        options += `<option value="${data[i].testTitle}">${data[i].testTitle}</option>`;
+                    };
+                    
+                    $('#dropdown-menu').append(options);
+                    
+                //$('#dropdown-menu').append(options);
                 console.log(data);    
             }
         });
     });
 };
 
+const viewTestDetails = () => {
+    $('option').one('click', event => {
+        event.preventDefault;
+        $.ajax({
+            url: '/tests/list',
+            type: 'get',
+            contentType: 'application/json',
+            success: function(data) {
+               let viewTest = 
+                `<h1>${data.testTitle}</h1>`;
+                $('.view-test').append(viewTest);
+            }
+        });
+    });
+}
+
+viewTests().then(viewTestDetails);
+
 function handleFunctions () {
     viewTests();
+    viewTestDetails();
 };
 
 $(handleFunctions);
