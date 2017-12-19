a8e8a347364ff6e790399a65560349445748cc35
Kevin Nichols <kevinlnichols@gmail.com>
2017-12-19 14:06:28 -0500

    updated test data

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server.js');
+const faker = require('faker');
+const mongoose = require('mongoose');
 
 const should = chai.should();
 const storage = server.storage;
@@ -8,24 +10,81 @@ const storage = server.storage;
 chai.use(chaiHttp);
 
 const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+const {User, Test, Question} = require('../models'); 
 
-describe('index', function() {
-    before(function() {
-        return runServer();
-      });
-    
-      after(function() {
-        return closeServer();
-      });
-
-    it('exists', function() {
-       /* chai.request(app)
-            .get('/')
-            .then(function(res, err) {
-                res.should.have.status(200);
-                res.should.be.html;
-                done();
-            });*/
-            return true;
+function tearDownDb() {
+    return new Promise((resolve, reject) => {
+      console.warn('Deleting database');
+      mongoose.connection.dropDatabase()
+        .then(result => resolve(result))
+        .catch(err => reject(err));
     });
+}
+
+function seedUserData() {
+    console.info('seeding user data');
+    const seedData = [];
+    for (let i = 1; i <= 10; i++) {
+      seedData.push({
+        name: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName()
+        },
+        username: faker.lorem.userName(),
+        password: faker.lorem.text(),
+        type: faker.lorem.words()
+      });
+    }
+    return User.insertMany(seedData);
+}
+
+function seedQuestionData() {
+    console.info('seeding question data');
+    const seedData = [];
+    for (let i = 1; i <= 10; i++) {
+      seedData.push({
+        title: faker.lorem.sentence(),
+        answerChoices: {
+          choice1: faker.lorem.sentence(),
+          choice2: faker.lorem.sentence(),
+          choice3: faker.lorem.sentence(),
+          choice4: faker.lorem.sentence()
+        },
+        correctAnswer: faker.lorem.sentence(),
+      });
+    }
+    return Question.insertMany(seedData);
+}
+
+function seedTestData() {
+    console.info('seeding test data');
+    const seedData = [];
+    for (let i = 1; i <= 10; i++) {
+      seedData.push({
+        testTitle: faker.lorem.sentence()
+      });
+    }
+    return Test.insertMany(seedData);
+}
+
+
+describe('user API resource', function() {
+    before(function() {
+        return runServer(TEST_DATABASE_URL);
+      });
+
+    beforeEach(function() {
+        return seedUserData();
+    });
+
+    afterEach(function() {
+        return tearDownDb();
+    });
+    
+    after(function() {
+    return closeServer();
+    });
+
+    
 });
